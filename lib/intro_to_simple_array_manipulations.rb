600429b8f684e129f4d776aae5bed01ee7897d8c
Emmanuel Jose <emjose@gmail.com>
2020-07-16 03:00:13 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_concat(array1, array 2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq
+end
+
+def using_flatten
+end
+
+def using_delete
+end
+
+def using_delete_at
+end
